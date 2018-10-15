6107a5b8a8fc85833b78ee9e6eabcfb6fdd6b7f9
Hung Nguyen <nmhung1805@gmail.com>
2018-10-15 15:19:11 +0700

    + Chọn thể loại bằng Spinner + Chọn ngày bằng DatePicker

--- app/src/main/java/com/apitiny/administrator/cinema_hung/Upload.kt
+++ app/src/main/java/com/apitiny/administrator/cinema_hung/Upload.kt
@@ -3,16 +3,35 @@ package com.apitiny.administrator.cinema_hung
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.upload.*
 
 class Upload : AppCompatActivity() {
+
+    lateinit var editTextDate: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.upload)
 
-        val theloai = arrayOf("Hành động", "Tâm lý", "Kinh dị", "Khoa học viễn tưởng", "Hài")
-        val theloai_onClick = findViewById<TextView>(R.id.tvtheloai)
-        //theloai_onClick.setOnClickListener
+        editTextDate = findViewById(R.id.ngayphathanh)
+    }
+
+    fun openPickerDate(view:View){
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH) + 1
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth -> editTextDate.setText("" + dayOfMonth + "/" + month + "/" + year)
+        }, year,month,day)
+        dpd.show()
     }
 }
